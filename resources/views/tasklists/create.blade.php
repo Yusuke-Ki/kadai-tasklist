02a17b80740f21e46924d0b5ba3f57876516a0a7
yusuke.kimura <yusuke.a.kimura@rakuten.com>
2018-05-28 03:33:34 +0000

    finish3

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -5,16 +5,23 @@
     <h1>タスクリスト新規作成ページ</h1>
 
 
-    {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+    <div class="row">
+        <div class="col-xs-12,col-sm-8,col-md-8,col-lg-6">
+            {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'タイトル:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
 
-    <h1>id: {{ $tasklist->id }} のメッセージ編集ページ</h1>
+    <h1>id: {{ $tasklist->id }} のタスク編集ページ</h1>
 
-    {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+    <div class="row">
+        <div class=""col-xs-12,col-sm-8,col-md-8,col-lg-6"">
+            {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'タイトル:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
